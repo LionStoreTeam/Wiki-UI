200a270f37950c6ab12157e39a4739d96ed226cd
LionStoreTeam <lionstoretechnologyteam@gmail.com>
2024-10-01 11:33:05 -0600

    Wiki UI 1 de Octubre 2024

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,60 +1,60 @@
-import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaClient } from "@prisma/client";
-import NextAuth from "next-auth/next";
-import bcrypt from "bcrypt";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import { PrismaClient } from "@prisma/client";
+// import NextAuth from "next-auth/next";
+// import bcrypt from "bcrypt";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-          placeholder: "someemail@email.com",
-          autocomplete: "off",
-        },
-        password: { label: "Password", type: "password", autocomplete: "off" },
-      },
-      async authorize(
-        credentials: any,
-        req: any
-      ): Promise<{ id: string; name: string; email: string }> {
-        const userFound = await prisma.user.findUnique({
-          where: {
-            email: credentials.email,
-          },
-        });
+// const handler = NextAuth({
+//   providers: [
+//     CredentialsProvider({
+//       name: "credentials",
+//       credentials: {
+//         email: {
+//           label: "Email",
+//           type: "email",
+//           placeholder: "someemail@email.com",
+//           autocomplete: "off",
+//         },
+//         password: { label: "Password", type: "password", autocomplete: "off" },
+//       },
+//       async authorize(
+//         credentials: any,
+//         req: any
+//       ): Promise<{ id: string; name: string; email: string }> {
+//         const userFound = await prisma.user.findUnique({
+//           where: {
+//             email: credentials.email,
+//           },
+//         });
 
-        if (!userFound) throw new Error("Usuario no encontrado");
+//         if (!userFound) throw new Error("Usuario no encontrado");
 
-        const passwordMatch = await bcrypt.compare(
-          credentials.password,
-          userFound.password
-        );
+//         const passwordMatch = await bcrypt.compare(
+//           credentials.password,
+//           userFound.password
+//         );
 
-        if (!passwordMatch) throw new Error("Contraseña incorrecta");
+//         if (!passwordMatch) throw new Error("Contraseña incorrecta");
 
-        return {
-          id: userFound.id.toString(),
-          name: userFound.name,
-          email: userFound.email,
-        };
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/auth/login",
-  },
-  callbacks: {
-    async session({ session, token, user }) {
-      // Guarda el userId en la sesión
-      session.userId = token.id as number;
-      return session;
-    },
-  },
-});
+//         return {
+//           id: userFound.id.toString(),
+//           name: userFound.name,
+//           email: userFound.email,
+//         };
+//       },
+//     }),
+//   ],
+//   pages: {
+//     signIn: "/auth/login",
+//   },
+//   callbacks: {
+//     async session({ session, token, user }) {
+//       // Guarda el userId en la sesión
+//       session.userId = token.id as number;
+//       return session;
+//     },
+//   },
+// });
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
